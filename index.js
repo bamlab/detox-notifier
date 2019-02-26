07e7a955efbedf14557913fafb0e35f923d28924
Louis Kraemer <louisk@bam.tech>
2019-02-26 13:22:58 +0100

    feat(index): cleaned index

--- index.js
+++ index.js
@@ -3,35 +3,15 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-const { initPushBulletStream, broadCastNote } = require("./pushbullet");
+require("./src/db");
+
+const { initPushBulletStream } = require("./src/pushbullet");
 
 initPushBulletStream();
 
 app.use(bodyParser.json());
 
-app.post("/detox/:project", async (req, res, next) => {
-  try {
-    const {
-      body: { tests }
-    } = req;
-    const areTestFailing = tests.reduce(
-      (acc, currentTest) => acc || currentTest.fail,
-      false
-    );
-    if (areTestFailing) {
-      const formattedTestResults = tests.reduce(
-        (acc, currentTest) =>
-          currentTest.fail ? `${acc}\n${currentTest.name} failed` : acc,
-        ""
-      );
-      await broadCastNote(req.params.project, formattedTestResults);
-    }
-    res.status(204).send();
-  } catch (error) {
-    console.log("error", error);
-    res.status(500).send();
-  }
-});
+require("./src/routes")(app);
 
 app.listen(6767, () => {
   console.log("Server running on port 6767");
