059d761cfd7a53ccb3d3b872632f7b733587c169
Louis Kraemer <louisk@bam.tech>
2019-02-27 22:13:20 +0100

    feat(db): automatic reconnection to db on startup error

--- src/db.js
+++ src/db.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
-mongoose
-  .connect("mongodb://mongo:27017", {
-    useNewUrlParser: false,
-    autoReconnect: true
-  })
-  .then(data => {
-    console.log("Connected to database");
-  })
-  .catch(err => {
-    console.log("Error : ", err);
-  });
+const initConnection = () =>
+  mongoose
+    .connect("mongodb://mongo:27017", {
+      useNewUrlParser: false,
+      autoReconnect: true
+    })
+    .then(() => console.log("Connected to database"))
+    .catch(err => {
+      console.log("Error connecting to database, retrying in 1 second");
+      setTimeout(initConnection, 1000);
+    });
+
+initConnection();
